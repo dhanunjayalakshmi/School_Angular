89fd6c387f94a5bb933cb49780f666aa5eac897e
Dhanunjaya Lakshmi <dhanunjaya.lakshmi60@gmail.com>
2016-04-09 18:18:30 +0530

    Delete a class

--- src/app/components/classes/classes.controller.js
+++ src/app/components/classes/classes.controller.js
@@ -6,12 +6,24 @@
     .controller('ClassesController', ClassesController);
 
   /** @ngInject */
-  function ClassesController($timeout, webDevTec, toastr, $resource) {
+  function ClassesController($timeout, webDevTec, toastr, $resource, $window) {
     var vm = this;
-
+    var classResource = $resource('http://localhost:3000/api/v1/klasses/:classId?access_token=J2JYA6HSHXKFVRNZY3G2JF4QXBBTX52P');
     vm.getClasses = function() {
-      var classResource = $resource('http://localhost:3000/api/v1/klasses?access_token=J2JYA6HSHXKFVRNZY3G2JF4QXBBTX52P');
       vm.classResponse = classResource.get();
+      console.log(vm.classResponse);
     }();
+
+    vm.addClass = function() {
+      var addClassResource = $resource('http://localhost:3000/api/v1/klasses?access_token=J2JYA6HSHXKFVRNZY3G2JF4QXBBTX52P');
+      vm.addClassResponse = addClassResource.get();
+    }();
+
+    vm.deleteClass = function(classes) {
+      console.log("delete");
+      console.log(classes);
+      classResource.delete({classId:classes.id,name:classes.name});
+      $window.location.href = '/';
+    }
   }
 })();
