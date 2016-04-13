16f58d74e654bb01d496eb6243af162e420134d3
Dhanunjaya Lakshmi <dhanunjaya.lakshmi60@gmail.com>
2016-04-13 14:43:16 +0530

    CRUD operations for Classes

--- src/app/components/sections/sections.controller.js
+++ src/app/components/sections/sections.controller.js
@@ -9,35 +9,38 @@
   function SectionsController($resource, $stateParams) {
     var vm = this;
 
+    var sectionResource = $resource('http://localhost:3000/api/v1/klasses/:classId/sections/:sectionId?access_token=J2JYA6HSHXKFVRNZY3G2JF4QXBBTX52P', {}, { 'update': {method: "PUT"}});
+
     vm.getSections = function() {
-      var sectionResource = $resource('http://localhost:3000/api/v1/klasses/:classId/sections/:sectionId?access_token=J2JYA6HSHXKFVRNZY3G2JF4QXBBTX52P', {}, { 'update': {method: "PUT"}});
-      vm.sectionResponse = sectionResource.get();
+      var classId = $stateParams.id
+      vm.sectionResponse = sectionResource.get({classId:  classId});
+      console.log(vm.sectionResponse);
     }();
 
     vm.addSection = function(name) {
       console.log("Add");
       console.log(name);
-      classResource.save({name:name});
+      sectionResource.save({name:name});
       $window.location.href = '/';
     }
 
-    vm.editSection = function(classes) {
+    vm.editSection = function(sections) {
       console.log("Edit");
       // console.log(classes.name);
       // console.log(classes.id);
       // vm.gotClass = classes.name;
       // vm.classId = classes.id;
-      vm.classes = classes;
+      vm.sections = sections;
       // console.log(vm.gotClass);
       $('#editModal').modal('show');
     }
 
-    vm.updateSection = function(classes, name) {
+    vm.updateSection = function(sections, name) {
       $('#editModal').modal('hide');
       console.log("Update");
       // console.log(classes.id);
       // console.log(name);
-      classResource.update({classId: classes.id, name: name}, classes);
+      sectionResource.update({classId: sections.klass_id, sectionId: sections.id, name: name}, sections);
       // $window.location.href = '/';
     }
 
@@ -45,7 +48,7 @@
       console.log("delete");
       console.log(sections);
       // classId = sections.klass_id
-      // sectionResource.delete({classId: classId, sectionId:sections.id, name:sections.name});
+      sectionResource.delete({classId: sections.klass_id, sectionId:sections.id, name:sections.name});
       // $window.location.href = '/classes/:classId/sections';
     }
 
